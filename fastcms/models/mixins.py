78d37446cb5c4df8db3cdc3a8da4a601a188f663
Azat <8280770+azataiot@users.noreply.github.com>
2025-06-09 18:22:18 +0300

    Add mixins for ULID primary keys, timestamps, and soft delete functionality

---
+++ fastcms/models/mixins.py
@@ -0,0 +1,87 @@
+# fastcms/models/mixins.py
+from sqlmodel import Field, DateTime
+from datetime import datetime
+from fastcms.utils import current_time
+from ulid import ULID
+
+
+class ULIDPrimaryKeyMixin:
+    """Mixin for models with ULID primary keys.
+    Fields:
+        - id: ULID primary key, stored as a string (CHAR(26)) in the database
+    """
+
+    # Stores ULID as a string (CHAR(26)) in the database
+    id: str | None = Field(default_factory=lambda: str(ULID()), primary_key=True)
+
+
+"""
+Note: without `type: ignore` PyCharm will complain about the type of `created_at` and `updated_at` fields:
+`Expected type 'type | PydanticUndefinedType', got 'DateTime' instead`
+"""
+
+
+class TimeStampMixin:
+    """Mixin for models with created_at and updated_at timestamps.
+    Fields:
+        - created_at: datetime when the record was created
+        - updated_at: datetime when the record was last updated
+    """
+
+    created_at: datetime = Field(
+        default_factory=current_time,
+        sa_type=DateTime(timezone=True),  # type: ignore
+    )
+    updated_at: datetime = Field(
+        default_factory=current_time,
+        sa_type=DateTime(timezone=True),  # type: ignore
+    )
+
+
+class PublishableMixin:
+    """Mixin for models with publishable fields.
+    Fields:
+        - is_published: boolean flag for published content
+        - published_at: datetime when the record was published
+        - unpublished_at: datetime when the record was unpublished
+    """
+
+    is_published: bool = Field(default=False)
+    published_at: datetime | None = Field(default=None, sa_type=DateTime(timezone=True))  # type: ignore
+    unpublished_at: datetime | None = Field(
+        default=None,
+        sa_type=DateTime(timezone=True),  # type: ignore
+    )
+
+
+class SlugMixin:
+    """Mixin for models with slug fields.
+    Fields:
+        - slug: string unique slug for the model
+    """
+
+    slug: str = Field(index=True, unique=True)  # Unique slug for the model
+
+
+class SoftDeleteMixin:
+    """Mixin for models with soft delete fields.
+    Fields:
+        - is_deleted: boolean flag for soft delete
+        - deleted_at: datetime when the record was softly deleted
+    """
+
+    is_deleted: bool = Field(default=False, index=True)  # Soft delete flag
+    deleted_at: datetime | None = Field(default=None, sa_type=DateTime(timezone=True))  # type: ignore
+
+
+class CommonFieldsMixin(ULIDPrimaryKeyMixin, TimeStampMixin, SoftDeleteMixin):
+    """Mixin for common table fields.
+    Fields:
+        - id: primary key (ULID)
+        - created_at: datetime when the record was created
+        - updated_at: datetime when the record was last updated
+        - is_deleted: boolean flag for soft delete
+        - deleted_at: datetime when the record was softly deleted
+    """
+
+    pass
--- fastcms/utils/__init__.py
+++ fastcms/utils/__init__.py
@@ -1,3 +1,4 @@
 # fastcms/utils/__init__.py
 
-from .text import slugify
+from .text import slugify  # noqa: F401
+from .time import current_time  # noqa: F401
